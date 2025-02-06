c1c5dfbf9265c2b1207d4a034ff3cd0cd929430d
Yuiko Koyanagi <yuiko@YuikonoMacBook-Pro.local>
2025-02-07 00:26:23 +0100

    fix:bug: update colour

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color(0xFFF6BD60);
   static const Color accent  = Color(0xFF84A59D);
-  static const Color buttonBackground = Color(0xFFF29E18);
+  static const Color buttonBackground = Color(0xFFF4A261);
   static const Color background = Color(0xFFF7EDE2);
-  static const Color textPrimary = Colors.black87;
+  static const Color textPrimary = Color(0xFF244357);
 }
